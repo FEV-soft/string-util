349d7a5b2d2083375286c1561072c7d7aa94d67c
FEV-soft <trafaret@trafaret.kiev.ua>
2020-08-14 23:53:28 +0300

    Add composer.lock Add Namespace

---
+++ src/StringUtil.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace ITEA;
+
+class StringUtil
+{
+    public static function toLowerCase($str)
+    {
+        return mb_strtolower($str);
+    }
+
+    public static function toUpperCase($str)
+    {
+        return mb_strtoupper($str);
+    }
+    
+    
+}
